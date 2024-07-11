13a574ec8739e346e8219714fb07b4dfa800367c
Iceheop <Iceheop@outlook.com>
2024-07-10 23:28:06 -0400

    initial commit

--- app/validation/validation.js
+++ app/validation/validation.js
@@ -1,29 +1,25 @@
-$(document).ready(function() {
-    $('#loginform').submit(function(e) {
-        e.preventDefault();
+$(document).ready(function () {
+  $("#loginform").submit(function (e) {
+    e.preventDefault();
 
-        var info = $(this).serialize();
+    var info = $(this).serialize();
 
-        $.ajax({
-            type: "POST",
-            url: 'app/validation/validation.php',
-            data: info,
-            success: function(response)
-            {
-                var jsonData = JSON.parse(response);
-                if (jsonData.success == "1")
-                {       
-                    window.location.href = ('app/project/administrador/index.php');
-                }
-                else if (jsonData.success == "0")
-                {
-                    swal({ 
-                        title: jsonData.text,
-                        text: jsonData.mensaje,
-                        icon: "error",
-                    })
-                }
-           }
-       });
-     });
+    $.ajax({
+        type: "POST",
+      url: "app/validation/validation.php",
+      data: info,
+      success: function (response) {
+        var jsonData = JSON.parse(response);
+        if (jsonData.success == "1") {
+          window.location.href = "app/project/administrador/index.php";
+        } else if (jsonData.success == "0") {
+          swal({
+            title: jsonData.text,
+            text: jsonData.mensaje,
+            icon: "error",
+          });
+        }
+      },
+    });
+  });
 });
